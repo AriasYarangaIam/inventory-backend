107e798867c65fe39001783c5dc0a0c9a3cc7dee
Iam Arias <fabricioayaranga@gmail.com>
2026-02-20 22:34:05 -0500

    editando dto del modelo movimiento

--- src/main/java/com/megaad/inventory/dto/MovimientoResponseDTO.java
+++ src/main/java/com/megaad/inventory/dto/MovimientoResponseDTO.java
@@ -2,18 +2,12 @@ package com.megaad.inventory.dto;
 
 
 import com.megaad.inventory.enums.TipoMovimiento;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class MovimientoResponseDTO {
 
     private Integer movimientoId;
